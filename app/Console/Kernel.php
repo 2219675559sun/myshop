d9064b7c2edcc78a427f624cf7817df90c2d322c
2219675559sun <2219675559@qq.com>
2019-08-26 16:33:06 +0800

    aa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,48 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call(function () {
+            $url="http://www.vizhiguo.com/qil/call";
+            $data=file_get_contents($url);
+            $data=json_decode($data,1);
+            $city = $data['result'];
+            $redis=new \Redis;
+            $redis->connect('127.0.0.1','6379');
+            foreach ($city as $k => $v) {
+//            查询存入redis里的数据
+                if($redis->exists($v['city'])){
+                    $city_info=json_decode($redis->get($v['city']),1);
+//                dump($city_info);
+//                如果数据更新查出来
+                    foreach($v as $k=>$val){
+                        if($val != $city_info[$k]){
+//                       模板推送
+                            $app=app('wechat.official_account');
+                            //查询用户
+                            $openid_list=$app->user->list($nextOpenId = null);
+                            $openid=$openid_list['data'];
+                            //循环展示用户的openid
+                            foreach($openid['openid'] as $k=>$v){
+
+                                $app->template_message->send([
+                                    'touser' =>$v,
+                                    'template_id' => 'MOtuco--HJATnyPCuegGKQR27e2u2NNycf4pyMpuX58-id',
+                                    'url' => 'https://easywechat.org',
+                                    'data' => [
+                                        'first' => $v['city'].'最新油价',
+                                        'keyword' => $v['city'] . '最新油价' . "\n" . 'b90:' . $v['b90'] . '￥' . "\n" . 'b93:' . $v['b93'] . '￥' . "\n" . 'b97:' . $v['b97'] . '￥' . "\n" . 'b0:' . $v['b0'] . '￥' . "\n" . '92h:' . $v['92h'] . '￥' . "\n" . '95h:' . $v['95h'] . '￥' . "\n" . '98h:' . $v['98h'] . '￥' . "\n" . '0h:' . $v['0h'] . '￥',
+                                        'keyword1'=>date('Y-m-d H:i:s',time()),
+                                        'remark'=>'如有打扰 尽情谅解',
+                                    ],
+                                ]);
+                            }
+                        }
+                    }
+                }
+            }
+//        })->daily();
+        })->everyMinute();
+
     }
 
     /**
