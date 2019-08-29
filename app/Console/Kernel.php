e63c31831c818df7b56c0416b315fc117b4fcbcc
2219675559sun <2219675559@qq.com>
2019-08-29 15:04:19 +0800

    aa

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use DB;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -41,50 +41,61 @@ class Kernel extends ConsoleKernel
                     ],
                 ]);
             }
+            //断签的话为0
+            $res=DB::connection('mysqls')->table('wechat_openid')->get()->toArray();
+            foreach($res as $v){
+                $res=DB::connection('mysqls')->table('wechat_openid')->where('openid',$v->openid)->first();
+                if($res->is_isset==0){
+                    $re=DB::connection('mysqls')->table('wechat_openid')->where('openid',$v->openid)->update([
+                        'aa'=>0,
+                    ]);
 
-
-
-           /* $redis=new \Redis;
-            $redis->connect('127.0.0.1','6379');
-            $app=app('wechat.official_account');
-
-            $url="http://www.vizhiguo.com/qil/call";
-            $data=file_get_contents($url);
-            $data=json_decode($data,1);
-            $city = $data['result'];
-            foreach ($city as $k => $v) {
-//            查询存入redis里的数据
-                if($redis->exists($v['city'])){
-                    $city_info=json_decode($redis->get($v['city']),1);
-//                dump($city_info);
-//                如果数据更新查出来
-                    foreach($v as $k=>$val){
-                        if($val != $city_info[$k]){
-//                       模板推送
-                            //查询用户
-                            $openid_list=$app->user->list($nextOpenId = null);
-                            $openid=$openid_list['data'];
-                            //循环展示用户的openid
-                            foreach($openid['openid'] as $k=>$va){
-                                $moban=$app->template_message->send([
-                                    'touser' =>$va,
-                                    'template_id' => 'MOtuco--HJATnyPCuegGKQR27e2u2NNycf4pyMpuX58',
-                                    'url' => 'http://www.vizhiguo.com/confession/add_confession',
-                                    'data' => [
-                                        'keyword' => $v['city'] . '最新油价' . "\n" . 'b90:' . $v['b90'] . '￥' . "\n" . 'b93:' . $v['b93'] . '￥' . "\n" . 'b97:' . $v['b97'] . '￥' . "\n" . 'b0:' . $v['b0'] . '￥' . "\n" . '92h:' . $v['92h'] . '￥' . "\n" . '95h:' . $v['95h'] . '￥' . "\n" . '98h:' . $v['98h'] . '￥' . "\n" . '0h:' . $v['0h'] . '￥',
-                                        'keyword1'=>date('Y-m-d H:i:s',time()),
-                                        'remark'=>'如有打扰 尽情谅解',
-                                    ],
-                                ]);
-                            }
-                        }
-                    }
                 }
-            }*/
+            }
+            /* $redis=new \Redis;
+             $redis->connect('127.0.0.1','6379');
+             $app=app('wechat.official_account');
+
+             $url="http://www.vizhiguo.com/qil/call";
+             $data=file_get_contents($url);
+             $data=json_decode($data,1);
+             $city = $data['result'];
+             foreach ($city as $k => $v) {
+ //            查询存入redis里的数据
+                 if($redis->exists($v['city'])){
+                     $city_info=json_decode($redis->get($v['city']),1);
+ //                dump($city_info);
+ //                如果数据更新查出来
+                     foreach($v as $k=>$val){
+                         if($val != $city_info[$k]){
+ //                       模板推送
+                             //查询用户
+                             $openid_list=$app->user->list($nextOpenId = null);
+                             $openid=$openid_list['data'];
+                             //循环展示用户的openid
+                             foreach($openid['openid'] as $k=>$va){
+                                 $moban=$app->template_message->send([
+                                     'touser' =>$va,
+                                     'template_id' => 'MOtuco--HJATnyPCuegGKQR27e2u2NNycf4pyMpuX58',
+                                     'url' => 'http://www.vizhiguo.com/confession/add_confession',
+                                     'data' => [
+                                         'keyword' => $v['city'] . '最新油价' . "\n" . 'b90:' . $v['b90'] . '￥' . "\n" . 'b93:' . $v['b93'] . '￥' . "\n" . 'b97:' . $v['b97'] . '￥' . "\n" . 'b0:' . $v['b0'] . '￥' . "\n" . '92h:' . $v['92h'] . '￥' . "\n" . '95h:' . $v['95h'] . '￥' . "\n" . '98h:' . $v['98h'] . '￥' . "\n" . '0h:' . $v['0h'] . '￥',
+                                         'keyword1'=>date('Y-m-d H:i:s',time()),
+                                         'remark'=>'如有打扰 尽情谅解',
+                                     ],
+                                 ]);
+                             }
+                         }
+                     }
+                 }
+             }*/
 //        dd($moban);
         })->daily();
 //        })->everyMinute();
 //    })->everyFiveMinutes();
+        $schedule->command('report:generate')
+            ->timezone('America/New_York')
+            ->at('13:05');
     }
 
     /**
