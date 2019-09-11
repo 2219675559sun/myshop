5d3e4285169796b8e9eb2dd9c87c17f073e1e5fb
2219675559sun <2219675559@qq.com>
2019-09-11 11:11:40 +0800

    aa

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::group(['middleware' => ['admin']], function () {
 Route::get('/index/logadd', 'index\IndexController@logadd');
 Route::get('/index/logadd_do', 'index\IndexController@logadd_do');
 //模板展示页面
-Route::get('/index', 'index\IndexController@index');
+Route::get('', 'index\IndexController@index');
 //模板退出登录
 Route::get('/index/sessionout', 'index\IndexController@sessionout');
 //模板详情页
